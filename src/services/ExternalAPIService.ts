636d1cd1d4a067dfcc786d05a1053e161c2321fd
Jake Snyder <jak3legacy@gmail.com>
2022-04-12 17:15:16 -0400

    Fixed main bugs and issues with matchup logic

--- src/services/ExternalAPIService.ts
+++ src/services/ExternalAPIService.ts
@@ -8,22 +8,29 @@ const rawGKey: string = `${process.env.REACT_APP_RAWG_KEY}` || "";
 
 // Searches for a list of popular movies,then picks a random page, then a specific result at random
 export const getMovie = (): Promise<MediaItem> => {
+  // randPageNum should be 1-62
+  let randPageNum = Math.floor(Math.random() * 62) + 1;
+  // randResultNum is for an array, so 1-20 actually equals 0-19
+  let randResultNum = Math.floor(Math.random() * 20);
+  console.log(
+    `Movie PageNum: ` + randPageNum,
+    `Movie ResultNum: ` + randResultNum
+  );
   let paramsObj = {
     api_key: tmdbKey as string,
     include_adult: "false",
     include_video: "false",
     language: "en-US",
-    "vote_count.gte": "3000",
-    page: (Math.floor(Math.random() * 62) + 1).toString(),
+    "vote_count.gte": "2990",
+    page: randPageNum.toString(),
   };
   return axios
     .get(`https://api.themoviedb.org/3/discover/movie`, {
       params: paramsObj,
     })
     .then((response) => {
-      const selection: any =
-        response.data.results[Math.floor(Math.random() * 20) + 1];
-      console.log(`Movie:`, selection);
+      let selection: any = response.data.results[randResultNum];
+      // console.log(`Movie:`, selection);
       const movie: MediaItem = {
         title: selection.title,
         subtitle: selection.release_date,
@@ -38,20 +45,28 @@ export const getMovie = (): Promise<MediaItem> => {
 
 // Searches for a list of TV shows, picks a random page, then picks a random result
 export const getTVShow = (): Promise<MediaItem> => {
+  // randPageNum should be 1-15
+  let randPageNum = Math.floor(Math.random() * 15) + 1;
+  // randResultNum is for an array, so 1-20 actually equals 0-19
+  let randResultNum = Math.floor(Math.random() * 20);
+  console.log(
+    `TVShow PageNum: ` + randPageNum,
+    `TVShow ResultNum: ` + randResultNum
+  );
   let paramsObj = {
     api_key: tmdbKey as string,
     language: "en-US",
-    "vote_count.gte": "1000",
-    page: (Math.floor(Math.random() * 14) + 1).toString(),
+    "vote_count.gte": "940",
+    page: randPageNum.toString(),
   };
   return axios
     .get(`https://api.themoviedb.org/3/discover/tv`, {
       params: paramsObj,
     })
     .then((response) => {
-      const selection: any =
-        response.data.results[Math.floor(Math.random() * 20) + 1];
-      console.log(`TV Show:`, selection);
+      // console.log(`TV Show Page:` + paramsObj.page);
+      let selection: any = response.data.results[randResultNum];
+      // console.log(`TV Show:`, selection);
       const show: MediaItem = {
         title: selection.name,
         subtitle: selection.first_air_date,
@@ -66,6 +81,7 @@ export const getTVShow = (): Promise<MediaItem> => {
 
 // Searches the met for an artpiece with a certain tag; cannot filter as robustly as others, so we need to search notable names and "similar"
 export const getArtpiece = (): Promise<MediaItem> => {
+  console.log(`Artpiece: No Problems Here`);
   let paramsObj = {
     q: "",
     hasImages: "true",
@@ -83,7 +99,7 @@ export const getArtpiece = (): Promise<MediaItem> => {
           `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artSelection}`
         )
         .then((response) => {
-          const selection: any = response.data;
+          let selection: any = response.data;
           const artpiece: MediaItem = {
             title: selection.title,
             subtitle: selection.artistDisplayName,
@@ -91,7 +107,7 @@ export const getArtpiece = (): Promise<MediaItem> => {
             category: "artpiece",
             nativeId: selection.objectID,
           };
-          console.log(`Artpiece:`, selection);
+          // console.log(`Artpiece:`, selection);
           return artpiece;
         });
     });
@@ -110,11 +126,19 @@ export const getAlbum = (): Promise<MediaItem> => {
     "punk",
     "rap",
   ];
+  // randTagNum looks at array length 9, rounds down to max of 8, making 0-8
+  let randTagNum = Math.floor(Math.random() * tagArray.length);
+  // randResultNum is for an array, so 1-50 actually equals 0-49
+  let randResultNum = Math.floor(Math.random() * 50);
+  console.log(
+    `Album TagNum: ` + randTagNum,
+    `Album ResultNum: ` + randResultNum
+  );
   let paramsObj = {
     api_key: lastFMKey as string,
     method: "tag.gettopalbums",
     format: "json",
-    tag: tagArray[Math.floor(Math.random() * tagArray.length)],
+    tag: tagArray[randTagNum],
     page: "1",
   };
   return axios
@@ -122,9 +146,8 @@ export const getAlbum = (): Promise<MediaItem> => {
       params: paramsObj,
     })
     .then((response) => {
-      const selection: any =
-        response.data.albums.album[Math.floor(Math.random() * 50) + 1];
-      const selectionImg = Object.entries(selection.image[3])[0];
+      let selection: any = response.data.albums.album[randResultNum];
+      let selectionImg = Object.entries(selection.image[3])[0];
       const album: MediaItem = {
         title: selection.name,
         subtitle: selection.artist.name,
@@ -132,32 +155,40 @@ export const getAlbum = (): Promise<MediaItem> => {
         category: "album",
         nativeId: `${selection.artist.name}: ${selection.name}`,
       };
-      console.log(`Album:`, selection);
+      // console.log(`Album:`, selection);
       return album;
     });
 };
 
 // Searches for acclaimed video games, chooses a random page, chooses a random result, and then searches for the resutls ID to gather deeper info
 export const getVideoGame = (): Promise<MediaItem> => {
+  // randPageNum has +1 because its not an array; needs range of 1-20
+  let randPageNum = Math.floor(Math.random() * 20) + 1;
+  // randResultNum is for an array, so 1-20 actually equals 0-19
+  let randResultNum = Math.floor(Math.random() * 20);
+  console.log(
+    `VidGame PageNum: ` + randPageNum,
+    `VidGame ResultNum: ` + randResultNum
+  );
   let paramsObj = {
     key: rawGKey as string,
     metacritic: "88,100",
     ordering: "-metacritic",
     exclude_additions: true,
-    page: (Math.floor(Math.random() * 20) + 1).toString(),
+    page: randPageNum.toString(),
   };
   return axios
     .get(`https://api.rawg.io/api/games`, {
       params: paramsObj,
     })
     .then((response) => {
-      let gameID = response.data.results[Math.floor(Math.random() * 20) + 1].id;
+      let gameID = response.data.results[randResultNum].id;
       return axios
         .get(`https://api.rawg.io/api/games/${gameID}`, {
           params: { key: rawGKey as string },
         })
         .then((response) => {
-          const selection: any = response.data;
+          let selection: any = response.data;
           const videoGame: MediaItem = {
             title: selection.name,
             subtitle: selection.released,
@@ -166,7 +197,7 @@ export const getVideoGame = (): Promise<MediaItem> => {
             category: "videogame",
             nativeId: selection.id,
           };
-          console.log(`Video Game:`, response.data);
+          // console.log(`Video Game:`, response.data);
           return videoGame;
         });
     });
