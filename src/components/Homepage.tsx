15324e601a48ccee0ef0110a7f37b0a0e3a3a77a
daryl <thedarylyap@gmail.com>
2022-04-13 11:58:23 -0400

    basic matchup display complete

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -9,25 +9,27 @@ import {
   getVideoGame,
 } from "../services/ExternalAPIService";
 import "./Homepage.css";
+import MatchupCard from "./MatchupCard";
 
 const Homepage = () => {
   const [matchups, setMatchups] = useState<Matchup[]>([]);
-  const [match1, setMatch1] = useState<MediaItem>({
-    title: "",
-    subtitle: "",
-    artImg: "",
-    artImg2: "",
-    category: "",
-    nativeId: "",
-  });
-  const [match2, setMatch2] = useState<MediaItem>({
-    title: "",
-    subtitle: "",
-    artImg: "",
-    artImg2: "",
-    category: "",
-    nativeId: "",
-  });
+  const [matchup, setMatchup] = useState<Matchup>();
+  // const [match1, setMatch1] = useState<MediaItem>({
+  //   title: "",
+  //   subtitle: "",
+  //   artImg: "",
+  //   artImg2: "",
+  //   category: "",
+  //   nativeId: "",
+  // });
+  // const [match2, setMatch2] = useState<MediaItem>({
+  //   title: "",
+  //   subtitle: "",
+  //   artImg: "",
+  //   artImg2: "",
+  //   category: "",
+  //   nativeId: "",
+  // });
   const getMediaArray = [
     getAlbum,
     getArtpiece,
@@ -50,50 +52,55 @@ const Homepage = () => {
 
     let media1 = await generateMedia(randSelection);
     let media2 = await generateMedia(randSelection2);
-    if (
-      media1.title === null ||
-      undefined ||
-      "" ||
-      media1.subtitle === null ||
-      undefined ||
-      "" ||
-      media1.artImg === null ||
-      undefined ||
-      ""
-    ) {
-      console.log(`Media1 generated again due to missing info.`);
-      media1 = await generateMedia(randSelection);
-    }
-    if (
-      media2.title === null ||
-      undefined ||
-      "" ||
-      media2.subtitle === null ||
-      undefined ||
-      "" ||
-      media2.artImg === null ||
-      undefined ||
-      ""
-    ) {
-      console.log(`Media2 generated again due to missing info.`);
-      media2 = await generateMedia(randSelection2);
-    }
+    // if (
+    //   media1.title === null ||
+    //   undefined ||
+    //   "" ||
+    //   media1.subtitle === null ||
+    //   undefined ||
+    //   "" ||
+    //   media1.artImg === null ||
+    //   undefined ||
+    //   ""
+    // ) {
+    //   console.log(`Media1 generated again due to missing info.`);
+    //   media1 = await generateMedia(randSelection);
+    // }
+    // if (
+    //   media2.title === null ||
+    //   undefined ||
+    //   "" ||
+    //   media2.subtitle === null ||
+    //   undefined ||
+    //   "" ||
+    //   media2.artImg === null ||
+    //   undefined ||
+    //   ""
+    // ) {
+    //   console.log(`Media2 generated again due to missing info.`);
+    //   media2 = await generateMedia(randSelection2);
+    // }
     const endTime = Date.now() - startTime;
     console.log(
       `The 'generateMatchup' function took ${endTime} ms to complete.`
     );
-    setMatch1(media1);
-    setMatch2(media2);
+    // setMatch1(media1);
+    // setMatch2(media2);
     console.log(media1, media2);
+    setMatchup({
+      media1,
+      media2,
+    });
   };
 
   useEffect(() => {}, []);
 
   return (
     <div className="Homepage">
-      {matchups.map((matchup) => {
+      {/* {matchup.map((item) => {
         return <p>{matchup.uid}</p>;
-      })}
+      })} */}
+      <MatchupCard matchup={matchup} />
       <button onClick={generateMatchup}>GENERATE MATCHUP</button>
     </div>
   );
--- src/components/MatchupCard.tsx
+++ src/components/MatchupCard.tsx
@@ -1,28 +1,31 @@
-import MediaItem from "../models/MediaItem";
+import Matchup from "../models/Matchup";
 import "./MatchupCard.css";
 
 interface Props {
-  match1: MediaItem;
-  match2: MediaItem;
+  matchup: Matchup | undefined;
 }
 
-const MatchupCard = ({ match1, match2 }: Props) => {
-  let backgroundImg1 = match1.artImg2 ? match1.artImg2 : match1.artImg;
-  let backgroundImg2 = match2.artImg2 ? match2.artImg2 : match2.artImg;
+const MatchupCard = ({ matchup }: Props) => {
+  let backgroundImg1 = matchup?.media1.artImg2
+    ? matchup.media1.artImg2
+    : matchup?.media1.artImg;
+  let backgroundImg2 = matchup?.media2.artImg2
+    ? matchup?.media2.artImg2
+    : matchup?.media2.artImg;
   return (
     <div className="MatchupCard">
-      <div className="match1-container">
-        <img src={match1.artImg} alt={match1.title} />
-        <p className="match1-title">{match1.title}</p>
-        <p className="match1-subtitle">{match1.subtitle}</p>
-        <img src={backgroundImg1} alt={match1.title} />
+      <div className="media1-container">
+        <img src={matchup?.media1.artImg} alt={matchup?.media1.title} />
+        <p className="media1-title">{matchup?.media1.title}</p>
+        <p className="media1-subtitle">{matchup?.media1.subtitle}</p>
+        <img src={backgroundImg1} alt={matchup?.media1.title} />
       </div>
       <p className="vs">VS</p>
-      <div className="match2-container">
-        <img src={match2.artImg} alt={match2.title} />
-        <p className="match2-title">{match2.title}</p>
-        <p className="match2-subtitle">{match2.subtitle}</p>
-        <img src={backgroundImg2} alt={match2.title} />
+      <div className="media2-container">
+        <img src={matchup?.media2.artImg} alt={matchup?.media2.title} />
+        <p className="media2-title">{matchup?.media2.title}</p>
+        <p className="media2-subtitle">{matchup?.media2.subtitle}</p>
+        <img src={backgroundImg2} alt={matchup?.media2.title} />
       </div>
     </div>
   );
